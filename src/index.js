81843f9a71d189e25b038264617e328264ecce9e
Shin Changseon <skywing27@gmail.com>
2018-11-07 12:45:45 +0900

    feat: login, style: header added login feature and styled header area

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ api.interceptors.request.use(function (config) {
 });
 
 const templates = {
-
+  loginForm: document.querySelector('#login-form').content
 }
 
 const rootEl = document.querySelector('.root')
@@ -28,3 +28,31 @@ const rootEl = document.querySelector('.root')
 // 4. 내용 채우기
 // 5. 이벤트 리스너 등록하기
 // 6. 템플릿을 문서에 삽입
+
+async function drawLoginForm() {
+  // 1. 템플릿 복사
+  const frag = document.importNode(templates.loginForm, true)
+  // 2. 요소 선택
+  const loginFormEl = frag.querySelector('.login-form')
+  // 3. 필요한 데이터 불러오기
+  // 4. 내용 채우기
+  // 5. 이벤트 리스너 등록하기
+  loginFormEl.addEventListener('submit', async e => {
+    e.preventDefault()
+    const username = e.target.elements.username.value
+    const password = e.target.elements.password.value
+    const res = await api.post('/users/login', {
+      username,
+      password
+    })
+    localStorage.setItem('token', res.data.token)
+    // login test
+    alert('로그인 성공!')
+  })
+
+  // 6. 템플릿을 문서에 삽입
+  rootEl.textContent = ''
+  rootEl.appendChild(frag)
+}
+
+drawLoginForm()
